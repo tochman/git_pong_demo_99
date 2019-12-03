b84e830befdd28f8f7792d74565006c9fe89855b
Thomas <thomas@craftacademy.se>
2019-12-03 14:11:44 +0100

    Just having fun!
    Co-authored-by: glitch880 <eliasgranfeldt@live.se>
    Co-authored-by: Erik Holstensson <traktorerik@gmail.com>
    Co-authored-by: eletor@jpj.com <eletor@jpj.com>
    Co-authored-by: Marcus Marecek <marcus.marecek@gmail.com>
    Co-authored-by: Rasmus Jarenmark <a16rasja@student.his.se>
    Co-authored-by: matand@jpj.com <matand@jpj.com>
    Co-authored-by: Alexey Polishchuk <lesha9772@gmail.com>

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // debugger
+  let displayEl = document.getElementById('display')
+  displayEl.innerHTML = '<h3>Hello Silverspin Coders! We are all happy Unicorns!</h3>'
+  displayEl.style.color = 'pink'
+
+  displayEl.style.fontSize = 42
+  
+
+  let text2 = document.getElementById('newText')
+
+  text2.style.color = purple
+  text2.style.height = 10
+})
